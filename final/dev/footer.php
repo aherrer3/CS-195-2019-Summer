e3a8cd8a79166ed4445c830d93367334dadba02d
Nicholas R. Lawrence <nrlawren@uvm.edu>
2019-06-23 02:42:26 -0400

    improvments
    - reduced more repeat code, added code to top.php
    - implememt php var for business sting for easy change
    - added footer.php with copyright and contact info
    - Validated html errors

---
+++ final/dev/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <p>Contact information: <a href="mailto:fakeemail@gmail.com">
+  fakeemail@gmail.com</a>.</p>
+  <i> <?php echo $busName?> </i> &copy; <?php echo date("Y"); ?>
+</footer>
+</body>
+</html>
--- final/dev/index.php
+++ final/dev/index.php
@@ -1,33 +1,25 @@
 <?php
 include ("top.php")
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-  <div class="container">
-   <a class="navbar-brand" href="#">
-       <img src="images/favicon_io/fav.jpg" width="50" height="50" class="d-inline-block align-top" alt="">
-    Alexa's Tattoo Business
-  </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">Home
-                <span class="sr-only">(current)</span>
-              </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="aboutUs.php">About Us</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="gallery.php">Gallery</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+<li class="nav-item active">
+    <a class="nav-link" href="index.php">Home
+        <span class="sr-only">(current)</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="aboutUs.php">About Us</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="gallery.php">Gallery</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="contact.php">Contact</a>
+</li>
+</ul>
+</div>
+</div>
 </nav>
 
+<?php
+include("footer.php")
+?>
