a71dd16e820a9e6d5fcb5f3aed7a2cb0f36d2201
Nicholas R. Lawrence <nrlawren@uvm.edu>
2019-06-23 01:31:32 -0400

    making blank files

---
+++ final/dev/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<title>Tattoo Shop</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Alexa Herrera Condry">
+	<meta name="description" content="CS 195 Final Project">
+</head>
+<?php
+include("nav.php")
+?>
+	</html>
