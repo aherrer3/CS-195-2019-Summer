d07de4ace2ff186d297e8afbfaeb5bbbd9bddde6
Nicholas R. Lawrence <nrlawren@uvm.edu>
2019-06-23 02:48:18 -0400

    more improvements
    added php vars for page names for easier transtion in the future for each of the nav pages

--- final/dev/aboutUs.php
+++ final/dev/aboutUs.php
@@ -2,18 +2,18 @@
 include ("top.php")
 ?> 
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Home
+          <a class="nav-link" href="index.php"><?php $navPage1 ?>
                 <span class="sr-only">(current)</span>
               </a>
         </li>
         <li class="nav-item active">
-            <a class="nav-link" href="aboutUs.php">About Us</a>
+            <a class="nav-link" href="aboutUs.php"><?php $navPage2 ?></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="gallery.php">Gallery</a>
+            <a class="nav-link" href="gallery.php"><?php $navPage3 ?></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
+            <a class="nav-link" href="contact.php"><?php $navPage4 ?></a>
         </li>
       </ul>
     </div>
--- final/dev/index.php
+++ final/dev/index.php
@@ -1,25 +1,21 @@
 <?php
 include ("top.php")
-?>
-<li class="nav-item active">
-    <a class="nav-link" href="index.php">Home
-        <span class="sr-only">(current)</span>
-    </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="aboutUs.php">About Us</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="gallery.php">Gallery</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="contact.php">Contact</a>
-</li>
-</ul>
-</div>
-</div>
+?> 
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php"><?php $navPage1 ?>
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="aboutUs.php"><?php $navPage2 ?></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="gallery.php"><?php $navPage3 ?></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.php"><?php $navPage4 ?></a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
-
-<?php
-include("footer.php")
-?>
